4290da7ecdf4df7faeb67aaaaadd440453b5514e
Korin <1878464932@qq.com>
2023-08-23 18:16:55 +0800

    实现传入参数，优化代码

--- plugins/SearchScore/SearchScore.ts
+++ plugins/SearchScore/SearchScore.ts
@@ -7,28 +7,40 @@ import YAML from 'yaml'
 const file = fs.readFileSync('plugins/SearchScore/config.yaml', 'utf8')
 const config = YAML.parse(file)
 
-bot.on("message.private",(msg)=>{
-    if (msg.raw_message.indexOf("/找") !== -1) {
-        msg.friend.sendMsg("请在群聊使用哦")
-    }
-})
-
 bot.on("message.group", async function (msg) {
     // if (msg.member.uid !== admin.account) { return }     // TODO 中间件:命令权限
-    if (msg.raw_message.indexOf("/找") !== -1) {
-        const filename: string = msg.raw_message.replace('/找', '').trim()
+    if (msg.raw_message.includes("/找")) {
+        var select: number = 0
+        var filename: string = msg.raw_message.replace('/找', '').trim()
+        //输入  /找one-4
+        if (/\-\d+/.test(filename)) {
+            // select = filename
+            const parts = filename.split('-');
+            const extractedNumbers:Array<number> = [];
+
+            for (let i = 1; i < parts.length; i++) {
+                const number = parseInt(parts[i], 10);
+                if (!isNaN(number)) {
+                    extractedNumbers.push(number);
+                }
+            }
+            select = parseInt(extractedNumbers.map(number => number.toString()).join(''))
+            console.log("select: "+select)
+            filename = filename.replace(/-\d+/g, '')
+        }
         var is_find: boolean = false
-        var search_group_range:string = config.search_group_range
-        var search_file_range:string = config.search_file_range
-        var search_other:boolean = false
+        var search_group_range: string = config.search_group_range
+        var search_file_range: string = config.search_file_range
+        var search_other: boolean = false
+
 
         if (search_group_range === "all" || search_group_range === "data") {
             //搜索数据库群
-            for (let i in config.data_groups) {
-                let group: Group = await bot.pickGroup(config.data_groups[i])
-                let fid: string[] = await searchFile(filename, "pdf", config.data_groups[i],search_file_range)
-                if (fid[0] !== "-1") {
-                    let filestat: GfsFileStat | GfsDirStat = await group.fs.stat(fid[0])
+            for (let i in config.data_group_list) {
+                let group: Group = await bot.pickGroup(config.data_group_list[i])
+                let fid: string[] = await searchFile(filename, "pdf", config.data_group_list[i], search_file_range)
+                if (fid[select] !== "-1") {
+                    let filestat: GfsFileStat | GfsDirStat = await group.fs.stat(fid[select])
                     msg.group.fs.forward(filestat as GfsFileStat)
                     is_find = true
                     break
@@ -37,19 +49,21 @@ bot.on("message.group", async function (msg) {
         }
 
         if (!is_find && search_group_range === "all") {
-            msg.group.sendMsg("数据库未收录，扩大查找范围")
+            // msg.group.sendMsg("数据库未收录，扩大查找范围")
             search_other = true
         }
 
         if (search_other || search_group_range === "other") {
             //搜索其他群
             for (let g of bot.gl.values()) {
-                if (config.data_groups.includes(g?.group_id)) continue // 跳过数据库群
+                if (config.data_group_list.includes(g?.group_id)) continue // 跳过数据库群
                 if (config.black_list.includes(g?.group_id)) continue
                 let group: Group = await bot.pickGroup(g?.group_id)
-                let fid: string[] = await searchFile(filename, "pdf", g?.group_id,search_file_range)
-                if (fid[0] !== "-1") {
-                    let filestat: GfsFileStat | GfsDirStat = await group.fs.stat(fid[0])    //TODO 插件配置文件:用户选择查看第几个文件
+                let fid: string[] = await searchFile(filename, "pdf", g?.group_id, search_file_range)
+                console.log(fid)
+                if (fid[select] !== "-1") {
+                    msg.group.sendMsg(`在${group.name}找到了`)
+                    let filestat: GfsFileStat | GfsDirStat = await group.fs.stat(fid[select])    //TODO 插件配置文件:用户选择查看第几个文件
                     msg.group.fs.forward(filestat as GfsFileStat)
                     is_find = true
                     break
@@ -67,13 +81,13 @@ bot.on("message.group", async function (msg) {
  * @param filename 文件名
  * @param ex_name 文件扩展名
  * @param group_id 群号
- * @param search_range `all` 为全部; `inDir` 在文件夹内搜索; `inRoot` 在根目录搜索
+ * @param search_range `all` 全部(默认); `inDir` 在文件夹内搜索; `inRoot` 在根目录搜索
  * @returns 所有结果文件的fid的集合，如果没有则返回["-1"]
 */
 async function searchFile(filename: string, ex_name: string, group_id: number, search_range: string = "all"): Promise<string[]> {
     return new Promise(async (resolve) => {
         var group: Group = await bot.pickGroup(group_id)
-        var group_files: (GfsDirStat | GfsFileStat)[] = await group.fs.dir()
+        var group_files: (GfsDirStat | GfsFileStat)[] = await group.fs.dir(...[, , 1000])
         var filestats_inRoot: GfsFileStat[] = []
         var filestats_inDir: GfsFileStat[] = []
         var filestats: GfsFileStat[] = []
@@ -82,13 +96,17 @@ async function searchFile(filename: string, ex_name: string, group_id: number, s
         var fullname: string = `${filename}.${ex_name}`
 
         for (let filestat of group_files) {
-            if (filestat.is_dir) promises.push(group.fs.dir(filestat.fid));
-            else filestats_inRoot.push(filestat as GfsFileStat);
+            if (filestat.is_dir) promises.push(group.fs.dir(...[filestat.fid, , 1000]));
+            else if ((filestat as GfsFileStat).name.endsWith(ex_name)) {
+                filestats_inRoot.push(filestat as GfsFileStat)
+            }
         }
         let filestats_dir = await Promise.all(promises);        //value [Dir1_filestats, Dir2_filestats, Dir3_filestats, ...]
         filestats_dir.forEach((filestats) => {
             filestats.forEach((filestat) => {
-                filestats_inDir.push((filestat as GfsFileStat))
+                if ((filestat as GfsFileStat).name.endsWith(ex_name)) {
+                    filestats_inDir.push((filestat as GfsFileStat))
+                }
             })
         })
         if (search_range === "all") filestats = filestats_inDir.concat(filestats_inRoot)
@@ -131,3 +149,9 @@ function compare(word1: string, word2: string): boolean {
     if (word1 === word2) return true;
     else return false
 }
+
+bot.on("message.private", (msg) => {
+    if (msg.raw_message.indexOf("/找") !== -1) {
+        msg.friend.sendMsg("请在群聊使用哦")
+    }
+})
