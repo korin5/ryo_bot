2fe05c9dbad8b419d8f576c7d29631fa53479ad3
korin5 <1878464932@qq.com>
2023-08-25 00:22:32 +0800

    other

--- plugins/Hello/Hello.ts
+++ plugins/Hello/Hello.ts
@@ -2,7 +2,7 @@ import { bot } from '../../src/test'
 
 bot.on("message", function (msg) {
     if (msg.raw_message === "ryo") {
-        msg.reply("Im alive!", true)
+        msg.reply("Im alive!", false)
     }
     if (msg.raw_message === "/help") {
         msg.reply("还没做呢", false)
