f4148d29415e578b04e0a7d94de51f2cb2ea58d1
Korin <1878464932@qq.com>
2023-08-24 16:35:18 +0800

    vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,18 @@
 module.exports = {
   title: 'ryo_bot',
-  description: '基于ICQQ开发的QQ机器人，这里是说明文档'
+  description: '基于ICQQ开发的QQ机器人，这里是说明文档',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '使用说明', link: '/use' },
+      { text: '开发文档', link: '/develop' },
+      // { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      '/',
+      '/page-a',
+      ['/page-b', 'Explicit link text']
+    ],
+    repo: 'korin5/ryo_bot',
+  }
 }
