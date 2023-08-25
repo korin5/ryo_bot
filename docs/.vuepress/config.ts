99491e7be4c10d0315aa832b5f34420ab7f4f81d
Korin <1878464932@qq.com>
2023-08-25 10:08:54 +0800

    多语言文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,48 +2,6 @@ module.exports = {
   title: 'ryo_bot',
   description: '基于ICQQ开发的QQ机器人，这里是说明文档',
   base: '/ryo_bot/',
-  themeConfig: {
-    locales: {
-      '/': {
-        selectText: "Language",
-        lable: "简体中文",
-        nav: [
-          { text: '使用说明', link: '/use' },
-          { text: '开发文档', link: '/develop' },
-        ],
-        repo: 'korin5/ryo_bot',
-        sidebar: 'auto',
-        smoothScroll: true
-      },
-      '/en/': {
-        selectText: "Language",
-        lable: "English",
-        nav: [
-          { text: 'How To Use', link: '/en/use' },
-          { text: 'Develop', link: '/en/develop' },
-        ],
-        repo: 'korin5/ryo_bot',
-        sidebar: 'auto',
-        smoothScroll: true
-      },
-      '/ja/': {
-        selectText: "Language",
-        lable: "日本語",
-        nav: [
-          { text: '使用方法', link: '/ja/use' },
-          { text: 'かいはつ', link: '/ja/develop' },
-        ],
-        repo: 'korin5/ryo_bot',
-        sidebar: 'auto',
-        smoothScroll: true
-      }
-    },
-    repo: 'korin5/ryo_bot',
-    // repoLabel: 'GitHub',
-  },
-
-
-
   locales: {
     '/': {
       lang: 'zh-CN',
@@ -54,5 +12,36 @@ module.exports = {
     '/ja/': {
       lang: 'ja-JP',
     }
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        label: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        nav: [
+          { text: '使用说明', link: '/use' },
+          { text: '开发文档', link: '/develop' },
+        ]
+      },
+      '/en/': {
+        label: 'English',
+        nav: [
+          { text: 'How To Use', link: '/en/use' },
+          { text: 'Develop', link: '/en/develop' },
+        ]
+      },
+      '/ja/': {
+        label: '日本語',
+        nav: [
+          { text: '使用方法', link: '/ja/use' },
+          { text: 'かいはつ', link: '/ja/develop' },
+        ]
+      },
+      repo: 'korin5/ryo_bot',
+      sidebar: 'auto',
+      smoothScroll: true
+    },
+    repo: 'korin5/ryo_bot',
+    // repoLabel: 'GitHub',
   }
 }
