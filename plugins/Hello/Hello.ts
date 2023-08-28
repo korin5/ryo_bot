45a0da073f1936dc41e864369adcccb0d99ab062
Korin <1878464932@qq.com>
2023-08-28 15:20:08 +0800

    Update Hello.ts

--- plugins/Hello/Hello.ts
+++ plugins/Hello/Hello.ts
@@ -1,7 +1,7 @@
 import { bot } from '../../src/test'
 
 bot.on("message", function (msg) {
-    if (msg.raw_message === "ryo") {
+    if (msg.raw_message === "/ryo") {
         msg.reply("Im alive!", false)
     }
     if (msg.raw_message === "/help") {
