543a86cbec29bdae0eb761cf239cf0ef33914391
Korin <1878464932@qq.com>
2023-08-29 16:07:57 +0800

    避免空指令

--- plugins/SearchScore/SearchScore.ts
+++ plugins/SearchScore/SearchScore.ts
@@ -13,7 +13,7 @@ bot.on("message.group", async function (msg) {
     //返回  ONE.pdf
     //输入  /找ONE-3
     //返回  第三个 ONE.pdf
-    if (msg.raw_message.includes("/找") && !/-\d+-\d+/.test(msg.raw_message)) {
+    if (msg.raw_message.includes("/找") && !/-\d+-\d+/.test(msg.raw_message) && msg.raw_message != '/找') {
         var [filename, arg_num] = await get_msg_info(msg.raw_message.replace('/找', ''))
         var select: number = arg_num <= 0 ? 0 : arg_num - 1
         var fid: string[] = []
